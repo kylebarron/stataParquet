07c435510435ab1812a31d4835934c8e6e26a933
Kyle Barron <barronk@mit.edu>
2018-11-01 17:55:50 -0400

    Rename Java files to ParquetReader

---
+++ src/main/java/com/kylebarron/stataParquet/ParquetReader.java
@@ -0,0 +1,37 @@
+package com.kylebarron.stataParquet;
+
+import com.stata.sfi.*;
+
+/**
+ * Read Parquet file into Stata
+ *
+ * @author Kyle Barron
+ * @version 0.0.0
+ */
+public class ParquetReader {
+
+    /**
+     * Boiler plate method that you can use quickly to get up and running.
+     * @param args This value is automatically obtained from the args()
+     *                option of the javacall command.  If possible, writting
+     *                your plugin to pass the parameters to this method will
+     *                make it easier to test/debug the Java side of things
+     *                independent of Stata.  To do that change 'int
+     *                entryPoint' to 'void main', recompile, and then you
+     *                should be able to run the same plugin from the command
+     *                line.
+     * @return The method must return an integer value to conform to the Java
+     * API expectations.  Non-zero values trigger errors (much the same way
+     * that non-zero returns in other compiled languages often indicate errors)
+     */
+    public static int methodUsedByJavacallCommand(String[] args) {
+
+        // Use this method to call class constructors and/or methods
+
+
+        // Returns a value of 0 on success
+        return 0;
+
+    } // End of Method declaration
+
+} // End of Class declaration
